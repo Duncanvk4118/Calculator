5841054488c998c7c7cc646b6c9a04d1b390b90e
Duncanvk4118 <97104118@st.deltion.nl>
2024-06-21 22:22:02 +0200

    Calculator app

---
+++ src/components/MyKeyboard.tsx
@@ -0,0 +1,186 @@
+import * as React from "react";
+import Button from "./Button";
+import { View, Text } from "react-native";
+import { Styles } from "../styles/GlobalStyles";
+import { myColors } from "../styles/colors";
+
+export default function MyKeyboard() {
+  const [firstNumber, setFirstNumber] = React.useState("");
+  const [secondNumber, setSecondNumber] = React.useState("");
+  const [operation, setOperation] = React.useState("");
+  const [result, setResult] = React.useState<Number | null>(null);
+  const [clickCount, setClickCount] = React.useState(0);
+  const timerRef = React.useRef<NodeJS.Timeout | null>(null);
+
+  const handleNumberPress = (buttonValue: string) => {
+    if (result !== null) {
+      setFirstNumber(buttonValue);
+      setResult(null);
+    } else if (firstNumber.length < 10) {
+      setFirstNumber(firstNumber + buttonValue);
+    }
+  };
+
+  const handleOperationPress = (buttonValue: string) => {
+    if (result !== null) {
+      setSecondNumber(result.toString());
+      setResult(null);
+    } else {
+      setSecondNumber(firstNumber);
+    }
+    setOperation(buttonValue);
+    setFirstNumber("");
+  };
+
+  const clear = () => {
+    setFirstNumber("");
+  };
+
+  const allClear = () => {
+    setFirstNumber("");
+    setSecondNumber("");
+    setOperation("");
+    setResult(null);
+  };
+
+  const handleClearPress = () => {
+    setClickCount((prev) => prev + 1);
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+    timerRef.current = setTimeout(() => {
+      if (clickCount >= 1) {
+        allClear();
+      } else {
+        clear();
+      }
+      setClickCount(0);
+    }, 300);
+  };
+
+  const firstNumberDisplay = () => {
+    if (result !== null) {
+      return (
+        <Text
+          style={
+            result < 999999
+              ? [Styles.screenFirstNumber, { color: myColors.result }]
+              : [
+                  Styles.screenFirstNumber,
+                  { fontSize: 50, color: myColors.result },
+                ]
+          }
+        >
+          {result?.toString()}
+        </Text>
+      );
+    }
+    if (firstNumber && firstNumber.length < 6) {
+      return <Text style={Styles.screenFirstNumber}>{firstNumber}</Text>;
+    }
+    if (firstNumber === "") {
+      return <Text style={Styles.screenFirstNumber}>{"0"}</Text>;
+    }
+    if (firstNumber.length > 5 && firstNumber.length < 8) {
+      return (
+        <Text style={[Styles.screenFirstNumber, { fontSize: 70 }]}>
+          {firstNumber}
+        </Text>
+      );
+    }
+    if (firstNumber.length > 7) {
+      return (
+        <Text style={[Styles.screenFirstNumber, { fontSize: 50 }]}>
+          {firstNumber}
+        </Text>
+      );
+    }
+  };
+
+  const getResult = () => {
+    if (secondNumber === "" || firstNumber === "") return;
+
+    let computation;
+    switch (operation) {
+      case "+":
+        computation = parseFloat(secondNumber) + parseFloat(firstNumber);
+        break;
+      case "-":
+        computation = parseFloat(secondNumber) - parseFloat(firstNumber);
+        break;
+      case "*":
+        computation = parseFloat(secondNumber) * parseFloat(firstNumber);
+        break;
+      case "/":
+        computation = parseFloat(secondNumber) / parseFloat(firstNumber);
+        break;
+      default:
+        return;
+    }
+    clear();
+    setResult(computation);
+    setFirstNumber(computation.toString());
+  };
+
+  return (
+    <View style={Styles.viewBottom}>
+      <View
+        style={{
+          height: 120,
+          width: "90%",
+          justifyContent: "flex-end",
+          alignSelf: "center",
+        }}
+      >
+        <Text style={Styles.screenSecondNumber}>
+          {secondNumber}
+          <Text style={{ color: "purple", fontSize: 50, fontWeight: "500" }}>
+            {operation}
+          </Text>
+        </Text>
+        {firstNumberDisplay()}
+      </View>
+      <View style={Styles.row}>
+        <Button
+          title={clickCount >= 1 ? "AC" : "C"}
+          isGray
+          onPress={handleClearPress}
+        />
+        <Button
+          title="+/-"
+          isGray
+          onPress={() => handleOperationPress("+/-")}
+        />
+        <Button title="％" isGray onPress={() => handleOperationPress("％")} />
+        <Button title="÷" isBlue onPress={() => handleOperationPress("/")} />
+      </View>
+      <View style={Styles.row}>
+        <Button title="7" onPress={() => handleNumberPress("7")} />
+        <Button title="8" onPress={() => handleNumberPress("8")} />
+        <Button title="9" onPress={() => handleNumberPress("9")} />
+        <Button title="×" isBlue onPress={() => handleOperationPress("*")} />
+      </View>
+      <View style={Styles.row}>
+        <Button title="4" onPress={() => handleNumberPress("4")} />
+        <Button title="5" onPress={() => handleNumberPress("5")} />
+        <Button title="6" onPress={() => handleNumberPress("6")} />
+        <Button title="-" isBlue onPress={() => handleOperationPress("-")} />
+      </View>
+      <View style={Styles.row}>
+        <Button title="1" onPress={() => handleNumberPress("1")} />
+        <Button title="2" onPress={() => handleNumberPress("2")} />
+        <Button title="3" onPress={() => handleNumberPress("3")} />
+        <Button title="+" isBlue onPress={() => handleOperationPress("+")} />
+      </View>
+      <View style={Styles.row}>
+        <Button title="." onPress={() => handleNumberPress(".")} />
+        <Button title="0" onPress={() => handleNumberPress("0")} />
+        <Button
+          title="⌫"
+          onPress={() => setFirstNumber(firstNumber.slice(0, -1))}
+        />
+        <Button title="=" isBlue onPress={getResult} />
+      </View>
+    </View>
+  );
+}
